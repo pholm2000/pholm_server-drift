fc481886aeb02e93eb2150a130a0e1eaa7d48ea2
pholm2000 <82170983+pholm2000@users.noreply.github.com>
2022-03-29 16:57:06 +0200

    Update sv_chat.lua

--- sv_chat.lua
+++ sv_chat.lua
@@ -1,6 +1,3 @@
---> 01001100 01100001 01110110 01100101 01110100 01011111 01000001 01100110 01011111 01110000 01101000 01101111 01101100 01101101 00100001 <--
--->                                                       Tal oven over må IKKE slettes!                                                   <--
-
 PerformHttpRequest('WEBHOOK', function(err, text, headers) end, 'POST', json.encode(
     {
         username = "SERVERNAVN - Server Drift", -- Bottens navn
